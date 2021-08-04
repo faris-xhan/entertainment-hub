c6478bdd5cd431ea7891eae8f21968965402da39
Evelyn <evelyn@protonmail.ch>
2021-08-04 07:06:16 -0700

    first commit

---
+++ src/privateHeaders.js
@@ -0,0 +1,7 @@
+const headers = {
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDdlYmRkNWE3NDkyMGMyMzI4YzcyNGI2MWI2N2FhOSIsInN1YiI6IjYxMGE2YmU2ZTYxZTZkMDAyMmRhYTI0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nf-vC2kNsA3g2jYWVYDsBbx69Dm-P0oDZDB1uSmwl9k",
+  "Content-Type": "application/json;charset=utf-8",
+};
+
+export default headers;
